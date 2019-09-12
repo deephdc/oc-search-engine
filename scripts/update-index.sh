7a3f2ec32ee72649ec16cab4bd76bdff93c486f6
Stevo Dlugolinsky <stevo@dlugolinsky.com>
2019-09-13 00:02:52 +0200

    updating index

---
+++ scripts/update-index.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+doc=$1
+
+#"&f=title:/title"\
+#"&f=summary:/summary"\
+
+curl 'http://127.0.0.1:8983/solr/open_catalog/update/json/docs'\
+'?f=title:/title'\
+'&f=summary:/summary'\
+'&f=description:/description'\
+'&f=keywords:/keywords'\
+'&f=license:/license'\
+'&f=date_creation:/date_creation'\
+	-H 'Content-type:application/json'\
+	-d @"$doc"
--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ docker run \
 	--rm \
 	-v $PWD/home:/opt/deep \
 	-v $PWD/solr.in.sh:/etc/default/solr.in.sh \
+	-v $PWD/exampledocs:/opt/exampledocs \
 	-p 8983:8983 \
 	-e SOLR_HOME=/opt/deep \
 	--name solr \
